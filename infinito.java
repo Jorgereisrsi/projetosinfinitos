38614e97c2c11c2243f36e61950b001dcb9ef86e
Jorgereisrsi <jorginhogdr@gmail.com>
2019-09-04 19:42:09 -0300

    Add files via upload

---
+++ infinito.java
@@ -0,0 +1,13 @@
+package x;
+
+public class Ion {
+
+      public static void main(String[] args) {
+		  init s = 0 ;
+		  for (byte b=0; b<128; b++) {
+			  s+=1;
+		  }
+		  
+		  System.out.println(s);
+		  
+	  }
