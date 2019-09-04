cc0a3781e96aeac2d74481576c4665fb9600b3f3
Jorgereisrsi <jorginhogdr@gmail.com>
2019-09-04 20:04:32 -0300

    Add files via upload

---
+++ soma.java
@@ -0,0 +1,26 @@
+Feed - 
+
+Foto Boa
+Vídeo Bom
+Posts com frase ou texto
+Equilíbrio entre o produzido de alta qualidade 
+Fotos em lugares Bonitos
+Estilo de Vida
+
+
+Stories
+
+Dia a dia
+indo tocar
+ensaio
+treino 
+bastidores
+brincadeira
+conversa 
+trabalhar mais a comunicação
+Coerente com a verdade - se estiver frio está frio/calor calor - Data de show Flyer !!!
+
+
+
+No Stories
+voce avisa que saiu clipe novo em outras redes e no feed
